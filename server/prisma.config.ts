3150b917236c1d4b1edfaf0a6312bb156b93d9f3
Iyonicorp <iyonicorp@gmail.com>
2026-05-16 13:55:42 +0300

    L

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 });
